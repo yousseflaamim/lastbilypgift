881b378f272e11bd9f1ace66ba9437c79843058b
yousseflaamim <39153351+yousseflaamim@users.noreply.github.com>
2022-11-27 16:34:27 +0100

    e

--- DumpaMera/src/Parking_A.java
+++
@@ -1,7 +0,0 @@
-public class Parking_A extends Parking{
-    @Override
-    public void NamePaking() {
-        System.out.println("welkom in DUMPA MERA you can parkera in park A");
-
-    }
-}
--- DumpaMera/src/Parking_E.java
+++
@@ -1,9 +0,0 @@
-public class Parking_E extends Parking{
-
-
-    @Override
-    public void NamePaking() {
-        System.out.println("welkom in DUMPA MERA you can parkera in park E");
-
-    }
-}
